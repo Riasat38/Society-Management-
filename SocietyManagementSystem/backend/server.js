f995e09b48c7d32f0f2cd859de0fcc6ae4f7f1d1
Ahmed Riasat <ahmedriasat3822@gmail.com>
2024-12-25 13:40:58 +0600

    jwt update

--- SocietyManagementSystem/backend/server.js
+++ SocietyManagementSystem/backend/server.js
@@ -34,8 +34,7 @@ app.use((req, res, next) => {
 app.use(cors({ 
   origin: 'http://localhost:3000', 
   methods: 'GET,POST,PUT,DELETE',  
-  allowedHeaders: 'Content-Type,Authorization', 
-  credentials: true  
+  allowedHeaders: 'Content-Type,Authorization'  
 }));
 
 app.use(session({
